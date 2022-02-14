c4c3366951ddad078a77dd1d167abe25d7480d6b
Ruijie218 <ruijie218@hotmail.com>
2022-02-14 21:03:52 +0800

    First Init HiWest
    Basic Implementation of HiWest

---
+++ src/occupy_gpu.py
@@ -0,0 +1,32 @@
+import os
+import torch
+from tqdm import tqdm
+import time
+
+# https://gist.github.com/sparkydogX/845b658e3e6cef58a7bf706a9f43d7bf
+
+# declare which gpu devices to use
+cuda_devices = '1,2,3'
+
+# Memory needed
+block_mem = 9000
+
+# Time for reservation (in seconds)
+reserved_time = 360000
+
+def occumpy_mem(cuda_device):
+    torch.cuda.set_device('cuda:'+cuda_device)
+    x = torch.cuda.FloatTensor(256, 1024, block_mem)
+    del x
+
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = cuda_devices
+    devices = cuda_devices.split(',')
+    for i in range(len(devices)):
+        print(f'Occupying Device No:', devices[i])
+        occumpy_mem(str(i))
+
+    for _ in tqdm(range(reserved_time)):
+        time.sleep(1)
+    print('Done')
---
+++ src/validate_script.sh
@@ -0,0 +1,1 @@
+ python train.py  -task ext  -mode test  -batch_size 500  -test_batch_size 500  -bert_data_path ../bert_data/cnndm  -model_path ./models  -test_from ./models/model_step_50000.pt  -log_file ../logs/test_ext_cnndm.log  -result_path ../results/ext_cnndm  -sep_optim true  -use_interval true  -visible_gpus 3  -max_pos 512  -max_length 200  -alpha 0.95  -min_length 50
